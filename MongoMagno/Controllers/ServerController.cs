10624ed8cd868062134966270aff102701a424c2
K. Scott Allen <scott@odetocode.com>
2013-08-11 14:30:08 -0400

    clean up

--- MongoMagno/Controllers/ServerController.cs
+++ MongoMagno/Controllers/ServerController.cs
@@ -8,14 +8,14 @@ namespace MongoMagno.Controllers
     public class ServerController : ApiController
     {         
         [GET("api/server/{server}")]
-        public IEnumerable<string> Get(string server)
+        public IEnumerable<string> GetDatabases(string server)
         {
             var db = new MongoDb(server);
             return db.GetDatabaseNames();
         }
 
         [GET("api/server/{server}/{database}")]
-        public IEnumerable<string> Get(string server, string database)
+        public IEnumerable<string> GetCollections(string server, string database)
         {
             var db = new MongoDb(server);
             return db.GetCollections(database);
