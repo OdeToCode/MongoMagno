dfb3945b2ebf54cdd1f14ba1991c62276be441c1
K. Scott Allen <scott@odetocode.com>
2013-08-13 20:23:03 -0400

    basic workspaces working

--- MongoMagno/Client/MMWorkspace/Scripts/mainController.js
+++ MongoMagno/Client/MMWorkspace/Scripts/mainController.js
@@ -4,8 +4,22 @@
     var MainController = function ($scope) {
         $scope.workspaces =
         [
-            { id: "workspace1", name: "Workspace 1" }
+            { id: "workspace1", name: "Workspace 1", selected:true }
         ];
+
+        $scope.addActiveIfSelected = function(workspace, existingClass) {
+            var result = existingClass || "";
+            if (workspace.selected) {
+                result += " active";
+            }
+            return result;
+        };
+
+        $scope.addWorkspace = function() {
+            var id = $scope.workspaces.length + 1;
+            angular.forEach($scope.workspaces, function (workspace) { workspace.selected = false });
+            $scope.workspaces.push({ id: "workspace" + id, name: "Workspace " + id, selected: true });
+        };
     };
 
     MainController.$inject = ["$scope"];
