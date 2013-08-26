abfe1dd274f33e042627f7c233fcf636e50be909
K. Scott Allen <scott@odetocode.com>
2013-08-26 15:11:09 -0400

    re-thinking how to execute queries

--- MongoMagno/Services/CommandExecutor.cs
+++ MongoMagno/Services/CommandExecutor.cs
@@ -1,13 +1,28 @@
-﻿using MongoDB.Driver;
+﻿using MongoMagno.Models;
 
 namespace MongoMagno.Services
 {
     public interface CommandExecutor
-    {       
+    {
+        void Execute(ClientCommand command, RouteMatchResult routeMatch);
     }
     
-    [CommandMatch("^db.(?<collection>\\w*).find\\(")]
+    [CommandMatch(@"^db.(?<collection>\w*).find\(")]
     public class FindExecutor : CommandExecutor
-    {        
+    {
+        private readonly IMongoDb _db;
+
+        public FindExecutor(IMongoDb db)
+        {
+            _db = db;
+        }
+
+        public void Execute(ClientCommand command, RouteMatchResult routeMatch)
+        {
+            var collectionName = routeMatch.Tokens["collection"];
+            //_db.Query(collectionName, )
+            
+
+        }
     }
 }
